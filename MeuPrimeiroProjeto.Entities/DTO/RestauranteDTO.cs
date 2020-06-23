7810834d565f55d4bf7bb8da69679a9bc3f825e3
Misaelrs10 <45718251+Misaelrs10@users.noreply.github.com>
2020-06-22 23:20:34 -0300

    site

---
+++ MeuPrimeiroProjeto.Entities/DTO/RestauranteDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeuPrimeiroProjeto.Entities.DTO
+{
+    public class RestauranteDTO
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Endereco { get; set; }
+
+        public string Votos { get; set; }
+
+    }
+}
--- MeuPrimeiroProjeto.Entities/Restaurante.cs
+++ MeuPrimeiroProjeto.Entities/Restaurante.cs
@@ -3,26 +3,26 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MeuPrimeiroProjeto.Entities
-{   
-    [Table ("RESTAURANTE")]
+{
+    [Table("RESTAURANTE")]
 
     public class Restaurante
     {
         [Key]
-        [Column ("REST_IDENT")]
+        [Column("REST_IDENT")]
 
         public long Id { get; private set; }
-        [Column ("REST_IDENT")]
+        [Column("REST_IDENT")]
 
         public String Name { get; private set; }
-        [Column ("REST_NOME")]
-
+        [Column("REST_NOME")]
 
         public String Endereco { get; private set; }
-        [Column ("REST_ENDERECO")]
+        [Column("REST_ENDERECO")]
 
         public String Votos { get; private set; }
         [Column("REST_VOTOS")]
 
+        protected Restaurante () {}
     }
 }
