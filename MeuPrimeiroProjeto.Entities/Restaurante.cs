3e09c1f23e3dbeb261e5ac9851014ae3a058965c
Misaelrs10 <45718251+Misaelrs10@users.noreply.github.com>
2020-06-27 19:04:55 -0300

    Revert "Update Restaurante.cs"
    This reverts commit b4480634f7dcab8e9dc570f1756ea3b8b2355bac.

--- MeuPrimeiroProjeto.Entities/Restaurante.cs
+++ MeuPrimeiroProjeto.Entities/Restaurante.cs
@@ -8,6 +8,9 @@ namespace MeuPrimeiroProjeto.Entities
 
     public class Restaurante
     {
+        private string restName;
+        private string restEndereco;
+        private int restVotos;
 
         [Key]
         [Column("REST_IDENT")]
@@ -15,13 +18,13 @@ namespace MeuPrimeiroProjeto.Entities
         public long Id { get; private set; }
         [Column("REST_IDENT")]
 
-        public String restName { get; private set; }
+        public String Name { get; private set; }
         [Column("REST_NOME")]
 
-        public String restEndereco { get; private set; }
+        public String Endereco { get; private set; }
         [Column("REST_ENDERECO")]
 
-        public int restVotos { get; private set; }
+        public String Votos { get; private set; }
         [Column("REST_VOTOS")]
 
         protected Restaurante () {}
